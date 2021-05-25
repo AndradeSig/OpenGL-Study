e0c3ac82e387e2574125b097c1e9b399523f743f
Andrade Sig <tssignorini@gmail.com>
2021-05-25 18:38:38 -0300

    Estudos OpenGL

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+/** PROTOTIPAÇÃO DAS FUNÇÕES: **/
+void frameBuffer_size_callback(GLFWwindow* window, int width, int height);
+void LoopRender(GLFWwindow* window);
+void processInput(GLFWwindow *window);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE); // Apenas ao MacOS X
+
+	/** CRIAR JANELA **/
+	GLFWwindow* window = glfwCreateWindow(800,600,"Janela OpenGL",NULL,NULL);
+	if(window == NULL){
+		std::cout << "Erro ao criar janela GLFW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	/** CRIAR O CONTEXTO DA JANELA **/
+	glfwMakeContextCurrent(window);
+
+
+	/** SETAR A VIEWPORT DA JANELA **/
+	glfwSetFramebufferSizeCallback(window,frameBuffer_size_callback);
+
+
+	/** SINALIZAR ERRO NO GLAD **/
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		std::cout << "Falha ao inicializar GLAD" << std::endl;
+		return -1;
+	}
+
+
+	/** SETAR O LOOP DE RENDERIZAÇÃO **/
+	LoopRender(window);
+
+	glfwTerminate(); // Deletar os recursos alocados
+	return 0;
+}
+
+void frameBuffer_size_callback(GLFWwindow* window, int width, int height){
+	glViewport(0,0,width,height);
+}
+
+void LoopRender(GLFWwindow* window){
+	while(!glfwWindowShouldClose(window)){
+
+		processInput(window); // Setar o Input
+		
+		// Renderização
+		glClearColor(0.2f,0.3f,0.3f,1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		// verifica e chama eventos e troca os buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+}
+
+void processInput(GLFWwindow *window){
+	if(glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS){
+		glfwSetWindowShouldClose(window,1); // Fechar ao clicar no botão "ESCAPE/ESC"
+	}
+}
+
